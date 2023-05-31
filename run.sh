fd2c2bd19d12b991456af6792c1b91c6c01a2986
Minh Bui <33414340+buiminhptit@users.noreply.github.com>
2023-05-31 17:54:10 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
 
+#!/bin/bash#
+python3 curio.py --data_path "data/merged_aq.json" --base_model "PygmalionAI/pygmalion-6b" \
+  --finetune_method 'qlora' --lora_r 8 --lora_alpha 32 --output_dir 'out/PygmalionCurio' \
+    --batch_size=128 --micro_batch_size 4 --cutoff_len 512 --num_epochs 3 --load_in_8bit False
